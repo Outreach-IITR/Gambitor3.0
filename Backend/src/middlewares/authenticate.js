42247b5782084519343b004d3d53ae7f37a1d082
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-18 05:06:11 +0530

    retrieve the profile from authorization header

--- Backend/src/middlewares/authenticate.js
+++ Backend/src/middlewares/authenticate.js
@@ -3,18 +3,19 @@ import jwt from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader === null || authHeader === undefined) {
-    return res.status(401).json({ status: 401, message: "UnAuthorized access" });
+    return res.status(401).json({ status: 401, message: "UnAuthorized access 1" });
   }
 
-  console.log("The token is", authHeader);
+  
 
   //removing the Bearer string
   const token = authHeader.split(" ")[1];
+  console.log("The token is", token);
 
   //   * Verify the JWT token
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
     if (err)
-      return res.status(401).json({ status: 401, message: "UnAuthorized access" });
+      return res.status(401).json({ status: 401, message: "UnAuthorized access 2" });
     req.user = user;
     next();
   });
--- Backend/src/routes/api.js
+++ Backend/src/routes/api.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import AuthController from "../controllers/authController.js";
-
+import authMiddleware from "../middlewares/authenticate.js";
+import ProfileController from "../controllers/profileController.js";
 
 const router = Router();
 
 router.post("/auth/register", AuthController.register);
 router.post("/auth/login", AuthController.login);
 
+// * Profile routes
+router.get("/profile", authMiddleware, ProfileController.index); //Private route
 
 export default router;
