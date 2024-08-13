c94e72d6027870ed1fdb6f145ab73cf1c7b420a7
revv11 <anand.utkarsh18@gmail.com>
2024-08-13 22:20:45 +0530

    changes on dashboard page

---
+++ Frontend/src/app/dashboard/Ambassador.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Ambassador(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
--- Frontend/src/app/dashboard/page.tsx
+++ Frontend/src/app/dashboard/page.tsx
@@ -1,11 +1,33 @@
-
+"use client"
+import { useState } from "react"
 import Image from "next/image"
 import Nav from "./nav"
 import Navbar from "./Navbar"
 import Home from "./home"
+import 'boxicons/css/boxicons.min.css';
+
+
+interface tabs{
+    text:string,
+    ico: string,
+}
+
+function Abc({text, ico}:tabs){
+    return(
+        <div className="flex items-center pl-6">
+            <i className={`${ico}`}></i>
+            <h1 className= "py-4 pl-3 cursor-pointer">{text}</h1>
+            
+        </div>
+    )
+}
 
 
 export default function Dashboard(){
+    const [nav , setNav] = useState("Dashboard")
+
+
+    const pages = [{text:"Dashboard", ico:'bx bxs-home-alt-2'}, {text:"Updates", ico:"bx bxs-bell"}, {text:"Practice Papers", ico:"bx bx-file"}, {text:"School Ambassador",ico:"bx bx-medal"}, {text:"Help Section", ico:"bx bxs-help-circle"}, {text:"Settings", ico:"bx bxs-cog"}]
     const updates = ["Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi",
                     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi,",
                     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, helu my frnd",
@@ -28,7 +50,14 @@ export default function Dashboard(){
                 </a>
                 
                 </div>
-                <Nav/>
+                <div className="lg:flex flex-col space-y-4 mt-[50px] ml-[3rem] text-[16px] font-medium  rounded-bl-xl rounded-tl-xl hidden">
+                {pages.map(({text,ico})=>(
+                <div onClick={()=>{setNav(text)}} key={text} className={nav==text?"bg-white rounded-bl-xl rounded-tl-xl": "bg-transparent text-white"}>
+                    <Abc text={text} ico={ico}/>
+
+                </div>
+                ))}
+        </div>
                 
                 
             </div>
